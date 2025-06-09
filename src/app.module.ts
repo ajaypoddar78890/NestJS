94daa95f0c1f279dff581c2fbaf47b75453c4643
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-06-09 07:14:16 +0530

    added the db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CarModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
+    CarModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
