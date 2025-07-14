c0ca05d5207fb16e68bbba21f51c4fe8e4fc97ab
anonymous78890 <ajay.poddar15fea@gmail.com>
2025-07-14 22:55:47 +0530

    added user api get and put api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schema/user,schema';
 
@@ -9,5 +9,15 @@ export class UserController {
   @Post('create')
   async create(@Body() body: any): Promise<User> {
     return await this.userService.createUser(body);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return await this.userService.getAllUsers();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() body: any): Promise<User> {
+    return await this.userService.updateUser(id, body);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,10 +5,30 @@ import { User, UserDocument } from './schema/user,schema';
 
 @Injectable()
 export class UserService {
+  async updateUser(id: string, body: any): Promise<User> {
+    const updatedUser = await this.userModel
+      .findByIdAndUpdate(id, body, { new: true })
+      .exec();
+    if (!updatedUser) {
+      throw new Error('User not found');
+    }
+    return updatedUser;
+  }
+  findAllUsers(): User[] | PromiseLike<User[]> {
+    throw new Error('Method not implemented.');
+  }
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async createUser(data: any): Promise<User> {
     const newUser = new this.userModel(data);
     return await newUser.save();
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async findByUserId(userID: string, data: any): Promise<User[]> {
+    return this.userModel.find({ userID: userID, ...data }).exec();
   }
 }
