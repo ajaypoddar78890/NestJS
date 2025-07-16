9833eb11a21993f793922a933e108437dde21a57
anonymous78890 <ajay.poddar15fea@gmail.com>
2025-07-16 13:02:22 +0530

    adde three request to Team

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get, Put, Param } from '@nestjs/common';
+import { Team } from './Schema/teams.schema';
+import { TeamsService } from './teams.service';
 
 @Controller('teams')
-export class TeamsController {}
+export class TeamsController {
+  constructor(private readonly teamsService: TeamsService) {}
+
+  @Post('create')
+  async createTeam(@Body() body: any): Promise<Team> {
+    return await this.teamsService.createTeam(body);
+  }
+  @Get()
+  async findAllTeam(): Promise<Team[]> {
+    return await this.teamsService.getAllTeams();
+  }
+
+  @Put(':id')
+  async updateTeam(@Param('id') id: string, @Body() body: any): Promise<Team> {
+    return await this.teamsService.updateTeam(id, body);
+  }
+}
--- src/teams/teams.service.ts
+++ src/teams/teams.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Team, TeamDocument } from './Schema/teams.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TeamsService {}
+export class TeamsService {
+  constructor(@InjectModel(Team.name) private teamModel: Model<TeamDocument>) {}
+
+  async createTeam(data: any): Promise<Team> {
+    const newTeam = new this.teamModel(data);
+    return newTeam.save();
+  }
+
+  async getAllTeams(): Promise<Team[]> {
+    return this.teamModel.find().exec();
+  }
+
+  async updateTeam(id: string, body: any): Promise<Team> {
+    const updatedTeam = await this.teamModel.findByIdAndUpdate(id, body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedTeam) {
+      throw new Error('Team not found');
+    }
+
+    return updatedTeam;
+  }
+}
